e65e12cd9a238655ba5a5c0e207005f78709be63
amol <amolswami2018@gmail.com>
2018-12-06 10:06:33 +0530

    Removed unused import

--- AllPayTest/src/main/java/com/allpay/qa/util/TestUtil.java
+++ AllPayTest/src/main/java/com/allpay/qa/util/TestUtil.java
@@ -1,19 +1,12 @@
 package com.allpay.qa.util;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 public class TestUtil {
